3f4de8b87d7170f5b6bc3886a4de16d9a14e0b48
nullmax <maxnull@hotmail.com>
2017-09-11 17:13:28 +0800

    性能优化
    减少synchronized关键字使用，提升性能

--- src/main/java/com/ele/util/Log.java
+++ src/main/java/com/ele/util/Log.java
@@ -1,13 +1,12 @@
 package com.ele.util;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Log {
-    private LinkedList<LogEntry> logEntries;    //for local state machine
-    private LinkedList<Integer> recentAppended;  //最近添加的日志，防止client未得到响应时重复提交
+    private ConcurrentHashMap<Integer, LogEntry> logEntries;    //for local state machine
+    private ConcurrentHashMap<Integer, Integer> recentAppended;  //最近添加的日志，防止client未得到响应时重复提交
 
     private String name;
 
@@ -16,6 +15,9 @@ public class Log {
     private boolean indexCacheDirty = true;
     private boolean termCacheDirty = true;
 
+
+    private AtomicInteger indexStart;
+    private AtomicInteger indexEnd;
     public int commitIndex;
     public int appliedIndex;
 
@@ -24,11 +26,14 @@ public class Log {
     public Log(int id) {
         buildLog(id);
         name = "log" + id;
-        logEntries = new LinkedList<LogEntry>();
-        recentAppended = new LinkedList<Integer>();
+        logEntries = new ConcurrentHashMap<Integer, LogEntry>();
+        recentAppended = new ConcurrentHashMap<Integer, Integer>();
 
         getLastIndex();
         getLastTerm();
+
+        indexStart = new AtomicInteger(lastIndexCache);
+        indexEnd = new AtomicInteger(lastIndexCache);
 
         commitIndex = lastIndexCache;
         appliedIndex = commitIndex;
@@ -44,7 +49,12 @@ public class Log {
     }
 
     private void buildLog(int id) {
-        String sql = "CREATE TABLE IF NOT EXISTS log" + id +
+        String sql = "CREATE TABLE IF NOT EXISTS simple" +
+                " (id int(11) DEFAULT NULL," +
+                " v int(11) DEFAULT NULL)";
+        DBConnector.update(sql);
+
+        sql = "CREATE TABLE IF NOT EXISTS log" + id +
                 " (term int(11) DEFAULT NULL," +
                 " logIndex int(11) DEFAULT NULL," +
                 " commandId int(11) DEFAULT NULL," +
@@ -56,7 +66,7 @@ public class Log {
         String queryString = "SELECT logIndex, commandId FROM " + name + " WHERE logIndex > " + temp + " ORDER BY logIndex ASC";
         List<Map<String, Object>> results = DBConnector.get(queryString);
         for (Map<String, Object> row : results) {
-            this.recentAppended.add((Integer) row.get("commandId"));
+            this.recentAppended.put((Integer) row.get("commandId"), (Integer) row.get("logIndex"));
         }
     }
 
@@ -67,42 +77,43 @@ public class Log {
         appliedIndex = (int) Index;
     }
 
-    public synchronized int getLastIndex() {
+    public int getLastIndex() {
         if (indexCacheDirty) {
             if (logEntries.isEmpty()) {
                 String queryString = "SELECT max(logIndex) AS lastLogIndex FROM " + name;
                 Object o = DBConnector.get(queryString).get(0).get("lastLogIndex");
                 lastIndexCache = (o == null) ? 0 : (Integer) o;
             } else {
-                lastIndexCache = logEntries.peekLast().logIndex;
+                lastIndexCache = indexEnd.get();
             }
             indexCacheDirty = false;
         }
         return lastIndexCache;
     }
 
-    public synchronized int getLastTerm() {
+    public int getLastTerm() {
         if (termCacheDirty) {
             if (logEntries.isEmpty()) {
                 String queryString = "SELECT term FROM " + name + " WHERE logIndex = " + getLastIndex();
                 List<Map<String, Object>> result = DBConnector.get(queryString);
                 lastTermCache = (result.isEmpty() ? 1 : (Integer) result.get(0).get("term"));
             } else {
-                lastTermCache = logEntries.peekLast().term;
+                lastTermCache = logEntries.get(getLastIndex()).term;
             }
             termCacheDirty = false;
         }
         return lastTermCache;
     }
 
-    public synchronized LogEntry getLogByIndex(int index) {
+    public LogEntry getLogByIndex(int index) {
         if (index == 0) {
             return new LogEntry(0, 0, -1, null);
         }
-        if (logEntries.isEmpty() || logEntries.peekFirst().logIndex > index) {
+
+        LogEntry logEntry = logEntries.get(indexStart.get());
+        if (logEntries.isEmpty() || logEntry != null && logEntry.logIndex > index) {
             String queryString = "SELECT * FROM " + name + " WHERE logIndex = " + index;
             List<Map<String, Object>> entryList = DBConnector.get(queryString);
-
             if (entryList.isEmpty()) {
                 return null;
             } else {
@@ -112,65 +123,66 @@ public class Log {
                         (Integer) entry.get("commandId"),
                         (String) entry.get("command"));
             }
+
         } else {
-            for (LogEntry logEntry : logEntries) {
-                if (logEntry.logIndex == index) {
-                    return logEntry;
-                }
-            }
+            return logEntries.get(index);
         }
-        return null;
     }
 
     // delete log from local state machine which index >= begin
-    public synchronized void deleteLogEntry(int begin) {
-        Iterator<LogEntry> it = logEntries.iterator();
-        while (it.hasNext()) {
-            LogEntry log = it.next();
-            if (log.logIndex >= begin) {
-                it.remove();
-            }
-        }
+    public void deleteLogEntry(int begin) {
+        for (int i = begin; i < indexEnd.get(); ++i)
+            logEntries.remove(i);
+        indexEnd.set(begin - 1);
         indexCacheDirty = true;
         termCacheDirty = true;
     }
 
-    public synchronized void addLogEntry(int term, String command, int commandId) {
-        int logIndex = getLastIndex() + 1;
+    public void addLogEntry(int term, String command, int commandId) {
+        int logIndex = indexEnd.incrementAndGet();
         LogEntry logEntry = new LogEntry(term, logIndex, commandId, command);
-        logEntries.add(logEntry);
+        logEntries.put(logIndex, logEntry);
         indexCacheDirty = true;
         termCacheDirty = true;
 
-        if (this.recentAppended.size() >= 5001) {
-            this.recentAppended.pollFirst();
+        if (recentAppended.size() >= 1000) {
+            Iterator it = recentAppended.entrySet().iterator();
+            it.next();
+            it.remove();
         }
-        this.recentAppended.add(commandId);
+        recentAppended.put(commandId, logIndex);
     }
 
     public synchronized void storeLog() {
         int counter = 0;
         while (counter < 100 && storedLogIndex < getLastIndex()) {
-            LogEntry logEntry = getLogByIndex(++storedLogIndex);
-            String sql = "INSERT INTO " + name + " VALUES (" + logEntry.term + "," + logEntry.logIndex + "," + logEntry.commandId + ",\'" + logEntry.command + "\')";
-            DBConnector.update(sql);
-            ++counter;
+            try {
+                LogEntry logEntry = getLogByIndex(++storedLogIndex);
+                String sql = "INSERT INTO " + name + " VALUES (" + logEntry.term + "," + logEntry.logIndex + "," + logEntry.commandId + ",\'" + logEntry.command + "\')";
+                DBConnector.update(sql);
+                ++counter;
+            } catch (NullPointerException npe) {
+                System.out.println(getLastIndex() + ", " + storedLogIndex);
+                System.exit(1);
+            }
         }
     }
 
     public synchronized void clearLog() {
-        Iterator<LogEntry> it = logEntries.iterator();
-        int endIndex = Math.min(storedLogIndex, appliedIndex);
-        while (it.hasNext()) {
-            if (it.next().logIndex < endIndex - 100) {
-                it.remove();
-            } else
-                break;
-        }
+        int endIndex = Math.min(storedLogIndex, appliedIndex) - 100;
+        for (int i = indexStart.get(); i < endIndex; ++i)
+            logEntries.remove(i);
+        indexStart.set(endIndex);
     }
 
     public boolean checkAppliedBefore(int commandId) {
-        if (recentAppended.contains(commandId))
+        if (recentAppended.get(commandId) != null && recentAppended.get(commandId) <= appliedIndex)
+            return true;
+        return false;
+    }
+
+    public boolean checkAppendBefore(int commandId) {
+        if (recentAppended.get(commandId) != null)
             return true;
         return false;
     }
